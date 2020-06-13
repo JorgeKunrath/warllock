0376cfcff9fa002400c30287ac4f16a0fcc2f15a
JorgeKunrath <jorgeks95@gmail.com>
2020-06-13 02:35:01 -0300

    create repository

---
+++ .eleventy.js
@@ -0,0 +1,40 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addWatchTarget("./assets/styles/");
+  eleventyConfig.addPassthroughCopy("./assets/styles/style.css");
+
+
+  return {
+    PassthroughFileCopy: true
+  }
+}
+
+
+
+// module.exports = function(eleventyConfig) {
+//   eleventyConfig.addWatchTarget("./src/styles/");
+// }
+
+//   return {
+//     dir: {
+//       input: "src",
+//       output: "dist"
+//     }
+//   };
+// };
+
+// module.exports = function(config) {
+
+//   config.addWatchTarget("./src/scss/");
+
+//   return {
+//     dir: {
+//       input: "src",
+//       output: "dist",
+//       //include: "templates"
+//     },
+//     templateFormats: ["html", "md"],
+//     htmlTemplateEngine: "liquid",
+//     markdownTemplateEngine: "liquid"
+//   };
+// }
